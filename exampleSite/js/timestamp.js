a214d00ebf95ea0516e8c24460b726517a55cf91
Andrew <andrewmjordan@users.noreply.github.com>
2022-02-08 14:36:54 -0800

    add more time information

--- exampleSite/js/timestamp.js
+++ exampleSite/js/timestamp.js
@@ -1,4 +1,4 @@
 var ts = document.getElementById("timestamp").textContent;
 var date = new Date(ts);
-
-document.getElementById("timestamp").textContent = moment(date).fromNow();
+document.getElementById("timestamp").textContent =moment(date).fromNow() ;
+document.getElementById("timestamp-secondary").textContent = moment(date).format('MMM D, HH:mm');
