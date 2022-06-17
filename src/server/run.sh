7c40091e7bc1c54d438e03e6716c97a153332819
Andrew <andrewmjordan@users.noreply.github.com>
2022-06-17 01:16:54 -0700

    fix variable substitution

--- src/server/run.sh
+++ src/server/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 envsubst < index.html > temp.html && mv temp.html index.html
+envsubst < js/params.js > temp.js && mv temp.js js/params.js
 envsubst < manifest.plist > temp.plist && mv temp.plist manifest.plist
 
 rm run.sh
