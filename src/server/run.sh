e63d330838b6dc244d91a6e807ddc4ff35d5acb7
Andrew <andrewmjordan@users.noreply.github.com>
2022-06-21 01:39:50 -0700

    fix run script

--- src/server/run.sh
+++ src/server/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 envsubst < site/index.html > temp.html && mv temp.html site/index.html
-envsubst < site/js/params.js > temp.js && mv temp.js site/js/params.js
 envsubst < site/manifest.plist > temp.plist && mv temp.plist site/manifest.plist
 
 caddy run
